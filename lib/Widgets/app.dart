a840064adc58fe92640119c67aae5a8e4ffa9939
Sergey <stolshin@yandex.ru>
2021-11-08 23:53:45 +0500

    Delete

--- lib/Widgets/app.dart
+++ lib/Widgets/app.dart
@@ -3,8 +3,8 @@ import 'package:my_fearst_flutter_proect/Widgets/user_profile.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-// commit
-  // commit 2
+// Delete
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: UserProfile()
