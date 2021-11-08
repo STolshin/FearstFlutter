bac7247afd6b179efe39f3939b92e2776bf20904
Sergey <stolshin@yandex.ru>
2021-11-08 23:35:19 +0500

    Freeeeeee

--- lib/Widgets/app.dart
+++ lib/Widgets/app.dart
@@ -3,7 +3,7 @@ import 'package:my_fearst_flutter_proect/Widgets/user_profile.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-
+// commit
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: UserProfile()
